88a1302b2192b953bb19db4d8a9b18d63b6884c2
Fernandas7 <maria.rodriguez@epn.edu.ec>
2026-05-23 23:00:10 -0500

    Agregado proyecto React

---
+++ React/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/header/Header"
+import Body1 from "./components/body1/body1"
+import About from "./components/about/About";
+import Destinations from "./components/destinations/Destinations";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Body1 />
+      <About />
+      <Destinations />
+    </>
+  );
+}
+
+export default App;
